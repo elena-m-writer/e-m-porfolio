197790470955fc18a7846d42aaa307b19fd61642
Elena <elena.m.writer@gmail.com>
2021-01-28 12:45:01 +0000

    Add colour change to JavaScript

--- learningJS.js
+++ learningJS.js
@@ -4,6 +4,9 @@ document.getElementById("code").addEventListener("mouseout", mouseOutCode);
 document.getElementById("type").addEventListener("mouseover", mouseOverType);
 document.getElementById("type").addEventListener("mouseout", mouseOutType);
 
+document.getElementById("javaScript").addEventListener("mouseover", mouseOverJavaScript);
+document.getElementById("javaScript").addEventListener("mouseout", mouseOutJavaScript);
+
 function mouseOverCode() {
   document.getElementById("code").style.color = "#fd691f";
 }
@@ -17,5 +20,13 @@ function mouseOverType() {
 }
 
 function mouseOutType() {
-  document.getElementById("type").style.color = "#013a33"
+  document.getElementById("type").style.color = "#013a33";
+}
+
+function mouseOverJavaScript() {
+  document.getElementById("javaScript").style.color = "#fd691f";
+}
+
+function mouseOutJavaScript() {
+  document.getElementById("javaScript").style.color = "#013a33";
 }
